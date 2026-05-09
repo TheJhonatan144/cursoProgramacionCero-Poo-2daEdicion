f6e2a823b607aa20d3b0b921282311b39276f3a4
Jhonatan Torres <jhonatan_pts144@hotmail.com>
2026-05-08 20:46:01 -0500

    Clase8-Crear plantilla para las funciones en vivo

---
+++ clase8/conceptos.cpp
@@ -0,0 +1,21 @@
+/*
+CONCEPTOS IMPORTANTES QUE DEBEMOS APRENDER
+
+1. Una funcion tiene una tarea especifica
+    NO conviene hacer todo dentro del main.
+
+2. Parámetros
+    Los parametros son lo datos que una funcion recibe
+    Ejemplo:
+    int calcularSuma(int a, int b, int c,...) TODO LO QUE ESTE DENTRO
+    DE LOS PARENTESIS SON PARAMETROS
+
+3. Retorno
+    El retorno es el valor que la funcion devuelve
+    Ejemplo:
+        return a+b
+
+4. Void
+    Una funcion void no devuelve valor.
+
+*/
