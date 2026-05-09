70476d158d561e61c83d59c2629f706961a1701f
Jhonatan Torres <jhonatan_pts144@hotmail.com>
2026-05-08 20:56:02 -0500

    clase8-crear archivo de ejercicio1 donde se representa funciones

--- clase8/ejercicio1.cpp
+++ clase8/ejercicio1.cpp
@@ -1,6 +1,29 @@
 /*
+EJERCICIO 1
 
-
-
+Autor: Jhonatan Torres
+Fecha: 08 de mayo de 2026
 
 */
+
+#include <iostream>
+using namespace std;
+
+// FUNCION PARA MODULARIZAR EL PROGRAMA
+void aumentar(int x)
+{
+    x = x + 1;
+    cout << "Dentro de la funcion, x vale: " << x << endl;
+}
+
+// ESTA ES LA FUNCION PRINCIPAL
+int main()
+{
+    int a = 10;
+
+    aumentar(a);
+
+    cout << "Fuera de la funcion, a vale: " << a << endl;
+
+    return 0;
+}
