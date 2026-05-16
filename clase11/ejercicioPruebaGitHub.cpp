74f75b0c3dc71dadc9b8c240fa3bc685ba74853c
Jhonatan Torres <jhonatan_pts144@hotmail.com>
2026-05-15 20:44:29 -0500

    Clase11-Crear archivo para prueba en github y archivos extra resagados.

---
+++ clase11/ejercicioPruebaGitHub.cpp
@@ -0,0 +1,14 @@
+// Clase 11
+/*
+Este es una prueba para mostrar los cambios en github
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "BIENVENIDOS AL CURSO";
+
+    return 0;
+}
